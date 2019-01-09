48753e894c97bd763bcdf2449915a14fe96f0bce
gabesinhorin <gabesinhorin@gmail.com>
2019-01-09 00:00:07 -0500

    Add files via upload

--- src/qa/parsers/JSONParser.java
+++ src/qa/parsers/JSONParser.java
@@ -18,7 +18,7 @@ public class JSONParser {
 	 * @param sourceString the string describing the source associated with these question
 	 * @return ArrayList<Question> the list of questions parsed from file
 	 */
-	public static ArrayList<Question> parseQald9File(String fileDirectory, String sourceString) {
+	public static ArrayList<Question> parseQald9File(String fileDirectory, String sourceString, String endpoint) {
 		ArrayList<Question> questionsList = new ArrayList<Question>();
 		System.out.println("Reading file " + fileDirectory + "...");
 		String fileContents = FileManager.readWholeFile(fileDirectory);
@@ -27,8 +27,10 @@ public class JSONParser {
 		JSONArray arr = obj.getJSONArray("questions");
 		for (int i = 0; i < arr.length(); i++) {
 			Question question = new Question();
+			question.setDatabase(endpoint);
 			// The source is predefined
 			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
 			JSONObject currentQuestionObject = arr.getJSONObject(i);
 			JSONArray objectArr =currentQuestionObject.getJSONArray("question"); 
 		    // Finding question in English
@@ -77,8 +79,8 @@ public class JSONParser {
 	 * @param sourceString the string describing the source associated with these question
 	 * @return ArrayList<Question> the list of questions parsed from file
 	 */
-	public static ArrayList<Question> parseQald8File(String fileDirectory, String sourceString) {
-		return parseQald9File(fileDirectory, sourceString);
+	public static ArrayList<Question> parseQald8File(String fileDirectory, String sourceString, String endpoint) {
+		return parseQald9File(fileDirectory, sourceString, endpoint);
 	}
 	/**
 	 * QALD-7 Parser for files: {qald-7-train-multilingual-extended-json.json}
@@ -87,17 +89,22 @@ public class JSONParser {
 	 * @param sourceString the string describing the source associated with these question
 	 * @return ArrayList<Question> the list of questions parsed from file
 	 */
-	public static ArrayList<Question> parseQald7File1(String fileDirectory, String sourceString) {
+	public static ArrayList<Question> parseQald7File1(String fileDirectory, String sourceString, boolean print, String endpoint) {
 		ArrayList<Question> questionsList = new ArrayList<Question>();
-		System.out.println("Reading file " + fileDirectory + "...");
+		if(print)
+			System.out.println("Reading file " + fileDirectory + "...");
+		
 		String fileContents = FileManager.readWholeFile(fileDirectory);
-		System.out.println("Parsing file...");
+		
+		if(print)
+			System.out.println("Parsing file...");
 		JSONObject obj = new JSONObject(fileContents);
 		JSONArray arr = obj.getJSONArray("questions");
 		for (int i = 0; i < arr.length(); i++) {
 			Question question = new Question();
 			// The source is predefined
 			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
 			JSONObject currentQuestionObject = arr.getJSONObject(i);
 			JSONObject questionObject =currentQuestionObject.getJSONObject("question");
 			JSONArray languageArr = questionObject.getJSONArray("language");
@@ -105,11 +112,13 @@ public class JSONParser {
 				if(languageArr.getJSONObject(j).getString("language").compareTo("en") == 0) {
 					question.setQuestionQuery(languageArr.getJSONObject(j).getString("SPARQL"));
 					question.setQuestionString(languageArr.getJSONObject(j).getString("question"));
-					System.out.println("Question " + i + ": " + question.getQuestionString());
+					if(print)
+						System.out.println("Question " + i + ": " + question.getQuestionString());
 					break;
 				}
 			}
 			JSONObject answers = questionObject.getJSONObject("answers");
+
 			JSONObject head = answers.getJSONObject("head");
 			
 			if(head.isEmpty()) {
@@ -143,7 +152,7 @@ public class JSONParser {
 	 * @param sourceString the string describing the source associated with these question
 	 * @return ArrayList<Question> the list of questions parsed from file
 	 */
-	public static ArrayList<Question> parseQald7File2(String fileDirectory, String sourceString) {
+	public static ArrayList<Question> parseQald7File2(String fileDirectory, String sourceString, String endpoint) {
 		ArrayList<Question> questionsList = new ArrayList<Question>();
 		System.out.println("Reading file " + fileDirectory + "...");
 		String fileContents = FileManager.readWholeFile(fileDirectory);
@@ -154,6 +163,7 @@ public class JSONParser {
 			Question question = new Question();
 			// The source is predefined
 			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
 			JSONObject currentQuestionObject = arr.getJSONObject(i);
 			String type = currentQuestionObject.getString("answertype");
 			JSONArray questionArray =currentQuestionObject.getJSONArray("question");
@@ -190,6 +200,7 @@ public class JSONParser {
 						variables.add(varsArray.getString(k));
 					}
 					JSONObject results = answer.getJSONObject("results");
+					
 					JSONArray bindingsArray = results.getJSONArray("bindings");
 					for(int k = 0; k < bindingsArray.length(); ++k) {
 						String answerString = "";
@@ -207,7 +218,151 @@ public class JSONParser {
 		}
 		return questionsList;
 	}
-	
+	/**
+	 * QALD-7 Parser for files: {qald-7-train-hybrid-extended-json.json,
+	 * 							./data/original/QALD-master/7/data/qald-7-train-multilingual-extended-json.json
+	 * }
+	 * 
+	 * @param fileDirectory the path to the file to be parsed
+	 * @param sourceString the string describing the source associated with these question
+	 * @return ArrayList<Question> the list of questions parsed from file
+	 */
+	public static ArrayList<Question> parseQald7File3(String fileDirectory, String sourceString, String endpoint) {
+		ArrayList<Question> questionsList = new ArrayList<Question>();
+		System.out.println("Reading file " + fileDirectory + "...");
+		String fileContents = FileManager.readWholeFile(fileDirectory);
+		System.out.println("Parsing file...");
+		JSONObject obj = new JSONObject(fileContents);
+		JSONArray arr = obj.getJSONArray("questions");
+		for (int i = 0; i < arr.length(); i++) {
+			Question question = new Question();
+			// The source is predefined
+			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
+			JSONObject currQuestion = arr.getJSONObject(i).getJSONObject("question");
+			
+			for(int j = 0; j < currQuestion.getJSONArray("language").length(); j++) {
+				if(currQuestion.getJSONArray("language").getJSONObject(j).getString("language").compareTo("en") == 0) {
+					try {
+						//System.out.print(currQuestion.getJSONArray("language").getJSONObject(j).getString("question"));
+						if(currQuestion.getJSONArray("language").getJSONObject(j).getString("SPARQL").isEmpty()) {
+							System.out.println("No query detected. Skipping this one!");
+							continue;
+						}
+					}
+					catch(Exception e){
+						System.out.println("No query detected. Skipping this file!");
+						return questionsList;
+					}
+					question.setQuestionString(currQuestion.getJSONArray("language").getJSONObject(j).getString("question"));
+				}	
+			}
+			//Get answers
+			ArrayList<String> answers = new ArrayList<String>();
+			if(currQuestion.getString("answertype").compareTo("resource") == 0) {
+				for(int j = 0; j < currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("uri").getString("value"));
+				}
+			}
+			else if(currQuestion.getString("answertype").compareTo("boolean") == 0) {
+				if(currQuestion.getJSONObject("answers").getBoolean("boolean"))
+					answers.add("true");
+				else
+					answers.add("false");
+			}
+			else if(currQuestion.getString("answertype").compareTo("date") == 0) {
+				for(int j = 0; j < currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("date").getString("value"));
+				}
+			}
+			else if(currQuestion.getString("answertype").compareTo("number") == 0) {
+				for(int j = 0; j < currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("c").getString("value"));
+				}
+			}
+			else if(currQuestion.getString("answertype").compareTo("string") == 0) {
+				for(int j = 0; j < currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(currQuestion.getJSONObject("answers").getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("string").getString("value"));
+					}
+				}
+			question.setAnswers(answers);
+			questionsList.add(question);
+		}
+		return questionsList;
+	}
+	/**
+	 * QALD-7 Parser for files: {qald-7-train-hybrid.json,
+	 * 			
+	 * }
+	 * 
+	 * @param fileDirectory the path to the file to be parsed
+	 * @param sourceString the string describing the source associated with these question
+	 * @return ArrayList<Question> the list of questions parsed from file
+	 */
+	public static ArrayList<Question> parseQald7File4(String fileDirectory, String sourceString, String endpoint) {
+		ArrayList<Question> questionsList = new ArrayList<Question>();
+		System.out.println("Reading file " + fileDirectory + "...");
+		String fileContents = FileManager.readWholeFile(fileDirectory);
+		System.out.println("Parsing file...");
+		JSONObject obj = new JSONObject(fileContents);
+		JSONArray arr = obj.getJSONArray("questions");
+		for (int i = 0; i < arr.length(); i++) {
+			Question question = new Question();
+			// The source is predefined
+			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
+			JSONArray currQuestion = arr.getJSONObject(i).getJSONArray("question");
+			
+			for(int j = 0; j < currQuestion.length(); j++) {
+					try {
+						//System.out.print(currQuestion.getJSONArray("language").getJSONObject(j).getString("question"));
+						if(arr.getJSONObject(i).getJSONObject("query").getString("pseudo").isEmpty()) {
+							System.out.println("No query detected. Skipping this one!");
+							continue;
+						}
+					}
+					catch(Exception e){
+						System.out.println("No query detected. Skipping this file!");
+						return questionsList;
+					}
+					question.setQuestionString(currQuestion.getJSONObject(j).getString("string"));
+			
+			}
+			//Get answers
+
+			ArrayList<String> answers = new ArrayList<String>();
+			if(arr.getJSONObject(i).getString("answertype").compareTo("resource") == 0) {
+				for(int j = 0; j < arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("uri").getString("value"));
+				}
+			}
+			
+			else if(arr.getJSONObject(i).getString("answertype").compareTo("boolean") == 0) {
+				if(arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getBoolean("boolean"))
+					answers.add("true");
+				else
+					answers.add("false");
+			}
+			else if(arr.getJSONObject(i).getString("answertype").compareTo("date") == 0) {
+				for(int j = 0; j < arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("date").getString("value"));
+				}
+			}
+			else if(arr.getJSONObject(i).getString("answertype").compareTo("number") == 0) {
+				for(int j = 0; j < arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("c").getString("value"));
+				}
+			}
+			else if(arr.getJSONObject(i).getString("answertype").compareTo("string") == 0) {
+				for(int j = 0; j < arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").length(); j++) {
+					answers.add(arr.getJSONObject(i).getJSONArray("answers").getJSONObject(0).getJSONObject("results").getJSONArray("bindings").getJSONObject(j).getJSONObject("string").getString("value"));
+					}
+				}
+			question.setAnswers(answers);
+			questionsList.add(question);
+		}
+		return questionsList;
+	}
 	/**
 	 * QALD-5 Parser for files: {qald-5_train.json}
 	 * 
@@ -215,7 +370,7 @@ public class JSONParser {
 	 * @param sourceString the string describing the source associated with these question
 	 * @return ArrayList<Question> the list of questions parsed from file
 	 */
-	public static ArrayList<Question> parseQald5(String fileDirectory, String sourceString) {
+	public static ArrayList<Question> parseQald5(String fileDirectory, String sourceString, String endpoint) {
 		ArrayList<Question> questionsList = new ArrayList<Question>();
 		System.out.println("Reading file " + fileDirectory + "...");
 		String fileContents = FileManager.readWholeFile(fileDirectory);
@@ -229,6 +384,7 @@ public class JSONParser {
 			Question question = new Question();
 			// The source is predefined
 			question.setQuestionSource(sourceString);
+			question.setDatabase(endpoint);
 			JSONObject currentQuestionObject = arr.getJSONObject(i);
 			String type = currentQuestionObject.getString("answertype");
 			JSONArray bodyArray =currentQuestionObject.getJSONArray("body");
--- src/qa/parsers/XMLParser.java
+++ src/qa/parsers/XMLParser.java
@@ -12,8 +12,63 @@ import org.w3c.dom.CharacterData;
 import qa.dataStructures.Question;
 
 public class XMLParser {
+
+	public static ArrayList<Question> parseQald4(String fileDirectory, String sourceString, String endpoint) {
+		ArrayList<Question> questionsList = new ArrayList<Question>();
+		try {
+	         File inputFile = new File(fileDirectory);
+	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	         System.out.println("Reading file " + fileDirectory + "...");
+	         Document doc = dBuilder.parse(inputFile);
+	         System.out.println("Parsing file...");
+	         doc.getDocumentElement().normalize();
+	         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+	         NodeList nList = doc.getElementsByTagName("question");
+	         
+	         for (int i = 0; i < nList.getLength(); i++) {
+	        	// Skip hybrid questions
+	        	Element element = (Element) nList.item(i);
+	            if(element.getAttribute("hybrid").compareTo("true") == 0) {
+	            	continue;
+	            }
+	        	Question question = new Question();
+	        	question.setQuestionSource(sourceString);
+	        	question.setDatabase(endpoint);
+	            NodeList langList = element.getElementsByTagName("string");
+	            for(int j = 0; j < langList.getLength(); ++j) {
+	            	Element langElement = (Element) langList.item(j);
+	            	if(langElement.getAttribute("lang").compareTo("en") == 0) {
+	            		question.setQuestionString(getCharacterDataFromElement(langElement));
+	            		break;
+	            	}
+	            }
+	            question.setQuestionQuery(getCharacterDataFromElement((Element)element.
+	            		getElementsByTagName("query").item(0)));
+	            Element answersNode = (Element) element.getElementsByTagName("answers").item(0);
+	            try {
+	            	NodeList answersList = answersNode.getElementsByTagName("answer");
+	            	
+	            	for(int j = 0; j < answersList.getLength(); ++j) {
+		            	
+		            	question.addAnswer(answersList.item(j).getTextContent());
+		            }
+	            }
+	            catch(Exception e)
+	            {
+	            	System.out.println("No answer found, use query");
+	            }
+	            
+	            
+	            questionsList.add(question);
+	         }
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+		return questionsList;
+	}
 	
-	public static ArrayList<Question> parseQald5(String fileDirectory, String sourceString) {
+	public static ArrayList<Question> parseQald5(String fileDirectory, String sourceString, String endpoint) {
 		ArrayList<Question> questionsList = new ArrayList<Question>();
 		try {
 	         File inputFile = new File(fileDirectory);
@@ -33,6 +88,7 @@ public class XMLParser {
 	            }
 	        	Question question = new Question();
 	        	question.setQuestionSource(sourceString);
+	        	question.setDatabase(endpoint);
 	            NodeList langList = element.getElementsByTagName("string");
 	            for(int j = 0; j < langList.getLength(); ++j) {
 	            	Element langElement = (Element) langList.item(j);
